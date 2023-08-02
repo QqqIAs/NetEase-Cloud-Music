32313b4faedaddc991c910d2011c82f0f7d13af7
QqqIAs <1982421402@qq.com>
2023-08-02 19:05:06 +0800

    fix: 时间换算错误

--- src/utils/getDurationTime.ts
+++ src/utils/getDurationTime.ts
@@ -1,7 +1,8 @@
 
 function getDurationTime(duration: number) {
+  // 179226
   const minutes = Math.floor(duration/60000)
-  const seconds = duration % 60
+  const seconds = Math.floor(duration/1000) % 60
   return `0${minutes}:${seconds < 10 ? '0'+seconds : seconds}`
 }
 
