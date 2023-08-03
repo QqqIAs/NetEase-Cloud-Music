5b03818670144c34640d9f5960232768fd4945a5
QqqIAs <1982421402@qq.com>
2023-08-04 00:50:04 +0800

    feat: 底部遮罩层&歌词抽屉搭建

--- src/components/PlayAudio/LeftAudioItem/index.tsx
+++ src/components/PlayAudio/LeftAudioItem/index.tsx
@@ -1,14 +1,41 @@
 import styles from './index.module.less'
 import formatTime from '@/utils/formatTime'
 import getDurationTime from '@/utils/getDurationTime'
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons'
+import { Drawer } from 'antd'
+import SongDetail from '@/components/SongDetail'
+import { useState } from 'react'
 
 function AudioItem (props) {
   
   const {picUrl, songName, artists, duration, currentTime} = props
 
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(!open);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
   return (
     <>
-        <img className={styles.img} src={picUrl} alt="" />
+        <Drawer rootClassName={styles.drawer} closeIcon={false} placement="bottom" onClose={onClose} open={open}><SongDetail></SongDetail></Drawer>
+        <div onClick={showDrawer} style={{ position: 'relative' }}>
+          <div className={styles.mask}>
+          {
+            open ? <>
+            <ArrowDownOutlined style={{ color: '#ffffff'}} />
+            <ArrowUpOutlined style={{ color: '#ffffff', marginBottom: '5px'}} /></>
+          : <>
+          <ArrowUpOutlined style={{ color: '#ffffff', marginBottom: '5px'}} />
+        <ArrowDownOutlined style={{ color: '#ffffff'}} /></>
+          }
+          </div>
+          <img className={styles.img} src={picUrl} alt="" />
+        </div>
         <div className={styles.text}>
           <div className={styles.top}>
             <span className={styles.name}>{songName}&nbsp;</span>
---
+++ src/components/SongDetail/index.tsx
@@ -0,0 +1,12 @@
+import styles from './index.module.less'
+
+function SongDetail () {
+
+  return (
+    <>
+      <div>123</div>
+    </>
+  )
+}
+
+export default SongDetail
